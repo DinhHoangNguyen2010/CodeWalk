6bd714704fba94a3ff2bd1a84f97759b503e3501
DinhHoangNguyen2010 <nguyenpc101010@gmail.com>
2025-02-18 11:15:55 +0700

    158B.cpp

--- 158B.cpp
+++ 158B.cpp
@@ -5,26 +5,22 @@
 #define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 using namespace std;
 int n,a[vip];
-main()
-{
+main() {
     cin>>n;
     for(int i=1; i<=n; i++)
         cin>>a[i];
     sort(a+1,a+n+1);
     int ans=0,s=0,c=n;
-    for(int i=1; i<=c; i++)
-    {
+    for(int i=1; i<=c; i++) {
         s=0;
-        s+=a[i]+a[c];
-        if(s>4)
-        {
+        s+=a[i]+a[c];   
+        if(s>4) {   ///if enough members,amount of car increase
             ans++;
             c--;
             i--;
             continue;
         }
-        while(s+a[i+1]<=4)
-        {
+        while(s+a[i+1]<=4) { ///else we add some members to the car
             i++;
             s+=a[i];
         }
@@ -33,5 +29,4 @@ main()
     }
     cout<<ans;
 }
-
 
